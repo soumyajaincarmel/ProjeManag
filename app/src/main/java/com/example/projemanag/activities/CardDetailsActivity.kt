430d724fe1fe0a161d99db4341a1114fc4cfe1ed
Soumya Jain <soumyajaincarmel@gmail.com>
2021-09-25 12:33:56 +0530

    Loading Card Details to set the Title of the Card.

--- app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/CardDetailsActivity.kt
@@ -2,11 +2,56 @@ package com.example.projemanag.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.example.projemanag.R
+import com.example.projemanag.models.Board
+import com.example.projemanag.utils.Constants
 
 class CardDetailsActivity : AppCompatActivity() {
+
+    private lateinit var mBoardDetails: Board
+
+    private var mTaskListPosition = -1
+    private var mCardListPosition = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_card_details)
+
+
+        getIntentData()
+        setUpActionBar()
+
+    }
+
+
+    private fun setUpActionBar() {
+        val toolbarCardDetailsActivity = findViewById<Toolbar>(R.id.toolbar_card_details_activity)
+        setSupportActionBar(findViewById(R.id.toolbar_card_details_activity))
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_white_color_back_24dp)
+            actionBar.title =
+                mBoardDetails.taskList[mTaskListPosition].cards[mCardListPosition].name
+        }
+
+        toolbarCardDetailsActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
+
+    private fun getIntentData() {
+        if (intent.hasExtra(Constants.BOARD_DETAIL)) {
+            mBoardDetails = intent.getParcelableExtra(Constants.BOARD_DETAIL)!!
+        }
+        if (intent.hasExtra(Constants.CARD_LIST_ITEM_POSITION)) {
+            mCardListPosition = intent.getIntExtra(Constants.CARD_LIST_ITEM_POSITION, -1)
+        }
+        if (intent.hasExtra(Constants.TASK_LIST_ITEM_POSITION)) {
+            mTaskListPosition = intent.getIntExtra(Constants.TASK_LIST_ITEM_POSITION, -1)
+        }
     }
 }
--- app/src/main/java/com/example/projemanag/utils/Constants.kt
+++ app/src/main/java/com/example/projemanag/utils/Constants.kt
@@ -31,6 +31,9 @@ object Constants {
 
     const val EMAIL: String = "email"
 
+    const val TASK_LIST_ITEM_POSITION: String = "task_list_item_position"
+    const val CARD_LIST_ITEM_POSITION: String = "card_list_item_position"
+
     //A unique code for asking the Read Storage Permission using this we will be check and identify in the method onRequestPermissionsResult
     const val READ_STORAGE_PERMISSION_CODE = 1
 
