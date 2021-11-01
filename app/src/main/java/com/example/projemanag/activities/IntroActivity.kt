4a5ca5d9890a10192a51c233c08898aac9771de2
Soumya Jain <soumyajaincarmel@gmail.com>
2021-11-01 11:43:30 +0530

    Replaced findViewById with ViewBinding in IntroActivity.kt.

--- app/src/main/java/com/example/projemanag/activities/IntroActivity.kt
+++ app/src/main/java/com/example/projemanag/activities/IntroActivity.kt
@@ -3,25 +3,28 @@ package com.example.projemanag.activities
 import android.content.Intent
 import android.os.Bundle
 import android.view.WindowManager
-import com.example.projemanag.R
+import com.example.projemanag.databinding.ActivityIntroBinding
 
 class IntroActivity : BaseActivity() {
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-        val btnSignUpIntro = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btn_sign_up_intro)
-        btnSignUpIntro.setOnClickListener{
+
+        binding.btnSignUpIntro.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
 
-        val btnSignInIntro = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.btn_sign_in_intro)
-        btnSignInIntro.setOnClickListener{
+
+        binding.btnSignInIntro.setOnClickListener {
             startActivity(Intent(this, SignInActivity::class.java))
         }
 
